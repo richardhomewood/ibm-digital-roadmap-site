a4288730aab169d1b42b0c7431b92d0d40179078
Richard Mattka <synergyseekers@gmail.com>
2022-04-12 00:42:30 -0400

    prototype svg build of atlas

--- src/components/Atlas/Atlas.js
+++ src/components/Atlas/Atlas.js
@@ -5,6 +5,7 @@ import { useTranslation } from 'react-i18next';
 import { Button } from  '@carbon/ibm-security';
 import { ButtonSet } from '@carbon/react';
 import { Trans } from 'react-i18next';
+import {  ReactComponent as AtlasSVG } from '../../assets/images/atlas.svg'
 
 function Atlas() {
 
@@ -13,15 +14,20 @@ function Atlas() {
 
     useEffect(() => {
         const tl = gsap.timeline({paused:true});
+        tl.set(".cls-1",{scale:0, svgOrigin:"300px 300px", transformOrigin:"-50% -50%"});
+        tl.to(".dot",{scale:0})
+        tl.to(".dot",{scale:2,duration:1.0,},.25)
+        tl.to(".dot",{scale:1,duration:1.0,},1.75)
+        tl.to([".circle-1", ".circle-2", ".circle-3" ],{stagger:.3, scale:1,duration:2, ease:"back.out"},0);
         // tl.to(['.loading-image',wallLogo.current ], {autoAlpha:0},0);
         tl.play();
     },[]);
-
+    
 
 
     return (
         <div className={`section atlas`}>
-            
+            <div className='atlas-container'><AtlasSVG /></div>
         </div>
     )
 }
