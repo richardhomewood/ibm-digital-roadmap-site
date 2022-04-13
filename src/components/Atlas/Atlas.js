d84a7c1ad881f0dcda7294ce69d2cf2269fc0cfd
Richard Mattka <synergyseekers@gmail.com>
2022-04-13 18:08:36 -0400

    add technology label to atlas

--- src/components/Atlas/Atlas.js
+++ src/components/Atlas/Atlas.js
@@ -2,19 +2,27 @@ import './Atlas.scss';
 import React, { useEffect, useRef, useState } from 'react'
 import gsap from "gsap";
 import { useTranslation } from 'react-i18next';
-import { Button } from  '@carbon/ibm-security';
-import { ButtonSet } from '@carbon/react';
-import { Trans } from 'react-i18next';
+// import { Button } from  '@carbon/ibm-security';
+// import { ButtonSet } from '@carbon/react';
+// import { Trans } from 'react-i18next';
 import {  ReactComponent as AtlasSVG } from '../../assets/images/atlas.svg'
+import { useNavigate } from 'react-router-dom';
 
 function Atlas() {
 
 
     const { t } = useTranslation();
+    const navigate = useNavigate();
 
     const dotClicked=(e)=>{
-        console.log("clicked", e.currentTarget.id)
-        
+        const id = e.currentTarget.id
+        const _t = id.charAt(id.length-1)
+        const _r = id.charAt(id.length-3)
+        const tech = t("header.technologies",{returnObjects: true})[_t].value
+        const year = t("atlas.years",{returnObjects:true})[_r-1].id
+        //const color = t("header.technologies",{returnObjects: true})[_t].color
+        console.log("over",id, tech, year )
+        navigate('/'+tech);
     }
 
     const dotOver=(e)=>{
@@ -39,7 +47,8 @@ function Atlas() {
     useEffect(() => {
         const tl = gsap.timeline({paused:true, delay:1});
         tl.set(".atlas-container",{autoAlpha:1});
-        tl.set(["#circle-1","#circle-2","#circle-3"],{scale:0, svgOrigin:"300px 300px", transformOrigin:"-50% -50%"});
+        tl.set(["#circle-1","#circle-2","#circle-3"],{scale:0, svgOrigin:"340px 340px", transformOrigin:"-50% -50%"});
+        tl.set(["#label-1", "#label-2","#label-3","#label-4", "#label-5","#label-6"],{scale:0, fillOpacity:0});
     
         // dynamaically update svg text as needed
         // document.querySelector("#year-1-text").innerHTML = "2025"
@@ -57,6 +66,7 @@ function Atlas() {
         }
        
         tl.to(["#circle-3", "#circle-2","#circle-1"  ],{stagger:.4, scale:1,duration:2.0, ease:"back.out"},0);
+        tl.to(["#label-1", "#label-2","#label-3","#label-4", "#label-5","#label-6" ],{stagger:.3, opacity:1, fillOpacity:1, scale:1, duration:1.0, ease:"back.out"},"-=.5");
         // tl.to(['.loading-image',wallLogo.current ], {autoAlpha:0},0);
         tl.play();
 
